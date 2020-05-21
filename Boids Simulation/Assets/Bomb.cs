76346a50c20ac161a03e7ef761ac9c171568ea9c
dotran98 <tranngocdo6111998@gmail.com>
2020-05-21 10:09:34 +1000

    first commit

---
+++ Boids Simulation/Assets/Bomb.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    float bombPower;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bombPower = 1000.0f;
+    }
+
+    void FixedUpdate()
+    {
+        Object.Destroy(this.gameObject, 1.0f);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Vector3 bomb = this.transform.position;
+        Vector3 target = col.gameObject.transform.position;
+
+        Vector3 force = target - bomb;
+
+        float dis = Vector3.Distance(bomb, target);
+
+        force = force.normalized * bombPower / dis;
+        col
+            .gameObject
+            .GetComponent<Rigidbody2D>()
+            .AddForce(force, ForceMode2D.Impulse);
+    }
+}
